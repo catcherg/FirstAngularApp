5d9da6526d8dfadc39139bc9e9b22fbd25f4ffaf
Catcher Griffin <catchergriffin@Catchers-Air-2.hsd1.co.comcast.net>
2024-07-16 14:43:37 -0600

    first commit

---
+++ homes-app/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+  `,
+  imports: [HomeComponent],
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'homes';
+}
+
